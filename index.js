5c65d47dd39a43ed925f3fb807c847c1ac725624
DeepaG <deepavishalig@gmail.com>
2023-03-16 00:51:08 +0530

    push

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 import express from "express";
+import * as dotenv from 'dotenv';
+import cors from "cors";
+import mongoose from 'mongoose';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+dotenv.config();
+
+//connecting with mongoose//
+mongoose
+.connect(process.env.MONGO_URL,{useNewUrlParser:true})
+.then(()=>console.log("Mongoose is connected🤣"))
+.catch((err)=>console.log("Error occured...Please check!😥",err))
+
+
+app.get('/',(req,res)=>{
+  res.send("This backend application is developed to create Student , create Mentor , To assign the students to a mentor , To assign a mentor to the students,etc...Please read the postman documention for better understanding ! Thankyou !😁😘 ")
+})
+
+app.listen(process.env.PORT,()=>console.log("Server started at the port",process.env.PORT))
